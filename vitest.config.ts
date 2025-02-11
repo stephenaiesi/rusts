21484fd9992318e4da0ddf41e12ed0db59d3badc
Stephen Aiesi <stephenaiesi@gmail.com>
2025-02-11 17:14:42 -0500

    fix: moved vitest config to root for vs_code testing tools to work

---
+++ vitest.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+	test: {
+		environment: "node",
+
+		globals: true,
+
+		include: ["tests/**/*.ts"],
+
+		coverage: {
+			provider: "v8",
+
+			include: ["src/option/**/*.ts"],
+			// include: ["src/**/*.ts"],
+
+			exclude: ["src/cli/run.ts"],
+
+			thresholds: {
+				lines: 100,
+				statements: 100,
+				branches: 100,
+				functions: 100,
+			},
+		},
+	},
+});
