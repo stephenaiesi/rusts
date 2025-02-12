222e11f0af1f73b1efbe88271dbcb464676d2595
Stephen Aiesi <stephenaiesi@gmail.com>
2025-02-12 13:34:23 -0500

    chore: added result module to tests coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 		coverage: {
 			provider: "v8",
 
-			include: ["src/option/**/*.ts"],
+			include: ["src/option/**/*.ts", "src/result/**/*.ts"],
 			// include: ["src/**/*.ts"],
 
 			exclude: ["src/**/types/**/*.ts", "src/**/types.ts"],
